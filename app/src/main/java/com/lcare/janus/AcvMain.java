531f4595b590e50be64db0ad5a872d83bcb51001
방상협 <chickengwang@gmail.com>
2016-11-30 08:58:21 +0900

    Initial commit

---
+++ app/src/main/java/com/lcare/janus/AcvMain.java
@@ -0,0 +1,13 @@
+package com.lcare.janus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AcvMain extends AppCompatActivity {
+
+	@Override
+	protected void onCreate( Bundle savedInstanceState ) {
+		super.onCreate( savedInstanceState );
+		setContentView( R.layout.acv_main );
+	}
+}
